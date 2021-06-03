3501e3f720932cbdaa552557b58c482ece209ced
Røhìt Síñgh Rajput <rs2001289@gmail.com>
2021-06-03 12:29:10 +0530

    progress

---
+++ Fyle1.py
@@ -0,0 +1,110 @@
+import os
+from flask import Flask, request, jsonify
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import json
+from flask_cors import CORS
+
+# db_host = os.environ['localhost']
+# db_name = os.environ['postgres']
+# db_user = os.environ['postgres']
+# db_URI = os.environ['postgres://postgres:Rohitszx@localhost:3000/postgres']
+# db_password = os.environ['Rohitszx']
+conn = psycopg2.connect(database="postgres", user="postgres", password="Rohitszx", host="127.0.0.1", port="3000")
+
+def autocomplete(keyword, limit, offset):
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    sql_query = f'''select
+                   *
+                   from
+                   branches
+                   where
+                   branch like '%{keyword}%'
+                   order by ifsc
+                   limit {limit} ; '''
+    cursor.execute(sql_query)
+    record = cursor.fetchall()
+
+    return record
+
+
+def getBranch(keyword, limit, offset):
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    sql_query = f'''
+                    select
+                *
+                from
+                branches
+                where
+                address like '%{keyword}%'
+                or branch like '%{keyword}%'
+                or city like '%{keyword}%'
+                or district like '%{keyword}%'
+                or state like '%{keyword}%'
+
+                order by
+                ifsc
+                limit
+                {limit}
+                offset {offset};
+            '''
+    cursor.execute(sql_query)
+    record = cursor.fetchall()
+
+    return record
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return '''
+            <h1>bad request!</h1>
+            Endpoint: /api/branches?q=<>
+            Endpoint: /api/branches/autocomplete?q=<>
+            ''', 400
+
+
+@app.route('/api/branches/autocomplete')
+def auto_branch():
+    search_query = request.args['q']
+
+    try:
+        limit = request.args['limit']
+    except:
+        limit = 5
+
+    try:
+        offset = request.args['offset']
+    except:
+        offset = 0
+    data = {}
+    data["branches"] = autocomplete(search_query.upper(), limit, offset)
+
+    return jsonify(data)
+
+
+@app.route('/api/branches')
+def branch():
+    search_query = request.args['q']
+
+    try:
+        limit = request.args['limit']
+    except:
+        limit = 5
+
+    try:
+        offset = request.args['offset']
+    except:
+        offset = 0
+    data = {}
+    data["branches"] = getBranch(search_query.upper(), limit, offset)
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Fyle1:app
